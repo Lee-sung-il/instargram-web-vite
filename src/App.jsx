43953b037aa91e5e34bc91420124cffa2b066b63
Lee-sung-il <wj132612@naver.com>
2025-05-29 11:25:23 +0900

    feat: 제목 변경

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>InstarGram-WebSite</h1>
+      <h1>InstarGram-WebSite-vite</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
